09066bf5099eea7631d0287727353dfd7e1551e3
Ivan <ivan.tranquilan@outlook.com>
2022-08-22 15:32:43 +0930

    sign up page almost complete

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Register.css";
 import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   function handleSubmit(event) {
@@ -14,39 +15,50 @@ const Register = () => {
       <div className="register-page__container">
         <h2>Register your account</h2>
         <form className="register__form" onSubmit={handleSubmit}>
-          <label htmlFor="username">Name</label>
-          <input
-            required
-            type="text"
-            className="form__input"
-            placeholder="Enter your name..."
-            name="username"
-          />
-          <label htmlFor="email">Email address</label>
-          <input
-            required
-            type="email"
-            className="form__input"
-            placeholder="Enter your email address..."
-            name="email"
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            required
-            type="password"
-            className="form__input"
-            placeholder="Enter your password..."
-          />
-          <label htmlFor="password-confirm">Confirm Password</label>
-          <input
-            required
-            type="password"
-            className="form__input"
-            placeholder="Enter your password again..."
-            name="password-confirm"
-          />
+          <div className="register__form--label-input">
+            <label htmlFor="username">Name</label>
+            <input
+              required
+              type="text"
+              className="form__input"
+              placeholder="Enter your name..."
+              name="username"
+            />
+          </div>
+          <div className="register__form--label-input">
+            <label htmlFor="email">Email address</label>
+            <input
+              required
+              type="email"
+              className="form__input"
+              placeholder="Enter your email address..."
+              name="email"
+            />
+          </div>
+          <div className="register__form--label-input">
+            <label htmlFor="password">Password</label>
+            <input
+              required
+              type="password"
+              className="form__input"
+              placeholder="Enter your password..."
+            />
+          </div>
+          <div className="register__form--label-input">
+            <label htmlFor="password-confirm">Confirm Password</label>
+            <input
+              required
+              type="password"
+              className="form__input"
+              placeholder="Enter your password again..."
+              name="password-confirm"
+            />
+          </div>
           <Button type="submit">Register</Button>
         </form>
+        <p className="sign-in">
+          Already have an account? <Link to="/login">Log In</Link>
+        </p>
       </div>
     </div>
   );
